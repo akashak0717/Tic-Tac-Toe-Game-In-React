4d9a9c570f40febdda759af4a929aecb50ece922
AKASH ARALIKATTI <akasharalikatti04@gmail.com>
2025-01-02 13:52:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './components/TicTacToe/TicTakToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
